e4d37d7c84761084d83158f621f5ea7d081d48c7
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2023-03-20 23:54:26 +0900

    set print in Walker2DBulletEnv class

--- pybulletgym/envs/roboschool/envs/locomotion/walker2d_env.py
+++ pybulletgym/envs/roboschool/envs/locomotion/walker2d_env.py
@@ -4,6 +4,7 @@ from pybulletgym.envs.roboschool.robots.locomotors import Walker2D
 
 class Walker2DBulletEnv(WalkerBaseBulletEnv):
     def __init__(self):
+        print("Walker2DBulletEnv.__init__() is called")
         self.robot = Walker2D()
         WalkerBaseBulletEnv.__init__(self, self.robot)
 
