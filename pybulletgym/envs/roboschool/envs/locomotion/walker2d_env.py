732ed8fe7c48fb338dfb9cd18a87a177eda84253
Taka Yoshida <pcho0712@gmail.com>
2023-03-20 19:55:04 +0900

    [obstacle] height 2.0m at x=5.0m

--- pybulletgym/envs/roboschool/envs/locomotion/walker2d_env.py
+++ pybulletgym/envs/roboschool/envs/locomotion/walker2d_env.py
@@ -6,9 +6,9 @@ class Walker2DBulletEnv(WalkerBaseBulletEnv):
     def __init__(self):
         self.robot = Walker2D()
         WalkerBaseBulletEnv.__init__(self, self.robot)
-        self.obstacle_pos_x = 2.0
+        self.obstacle_pos_x = 5.0
         self.obstacle_size_x = 1.0
-        self.obstacle_size_z = 0.5
+        self.obstacle_size_z = 2.0
 
     def collision_check(self)-> bool:
         if self.obstacle_pos_x < self.robot.body_xyz[0] < self.obstacle_pos_x+self.obstacle_size_x and self.robot.body_xyz[2] < self.obstacle_size_z:
