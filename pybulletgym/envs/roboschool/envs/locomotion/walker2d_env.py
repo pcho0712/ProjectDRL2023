d804cc50e41cdbe97f45c3edc9d0ac125845d001
Naoki-Take <63697416+Naoki-Take@users.noreply.github.com>
2023-03-22 12:58:10 +0900

    Update walker2d_env.py
    pos_x = 0.0, size_x = 30.0, size_z = 0.6

--- pybulletgym/envs/roboschool/envs/locomotion/walker2d_env.py
+++ pybulletgym/envs/roboschool/envs/locomotion/walker2d_env.py
@@ -8,7 +8,7 @@ class Walker2DBulletEnv(WalkerBaseBulletEnv):
         WalkerBaseBulletEnv.__init__(self, self.robot)
         self.obstacle_pos_x = 0.0
         self.obstacle_size_x = 30.0
-        self.obstacle_size_z = 1.05
+        self.obstacle_size_z = 0.6
 
     def collision_check(self)-> bool:
         if self.obstacle_pos_x < self.robot.body_xyz[0] < self.obstacle_pos_x+self.obstacle_size_x and self.robot.body_xyz[2] < self.obstacle_size_z:
