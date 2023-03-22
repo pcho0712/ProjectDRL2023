c69b25db65eb841d0408c5dc38d9b4949400d310
Naoki-Take <63697416+Naoki-Take@users.noreply.github.com>
2023-03-22 12:59:13 +0900

    Update walker_base_env.py
    -5.0*collision_reward

--- pybulletgym/envs/roboschool/envs/locomotion/walker_base_env.py
+++ pybulletgym/envs/roboschool/envs/locomotion/walker_base_env.py
@@ -101,7 +101,7 @@ class WalkerBaseBulletEnv(BaseBulletEnv):
             print(feet_collision_cost)
 
         self.rewards = [
-            5.0*collision_reward,
+            -5.0*collision_reward,
             alive,
             progress,
             electricity_cost,
