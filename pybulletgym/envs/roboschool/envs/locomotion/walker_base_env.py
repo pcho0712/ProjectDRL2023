3497e5c18cf9af11499695bf618dc11bc0bc3b5d
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2023-05-21 21:26:01 +0900

    [FOOT][SHOW_CONTACT_POS] show contact each points, and force of humanoids

--- pybulletgym/envs/roboschool/envs/locomotion/walker_base_env.py
+++ pybulletgym/envs/roboschool/envs/locomotion/walker_base_env.py
@@ -75,7 +75,9 @@ class WalkerBaseBulletEnv(BaseBulletEnv):
             self.robot.foot_2d_orien[i] = (R.from_quat(self.robot.foot_3d_orien[i]).as_matrix())[:2,2] # z direction is the normal of the foot (capsule)
             contact_ids = set((x[2], x[4]) for x in f.contact_list())
             # print("CONTACT OF '%d' WITH %d" % (contact_ids, ",".join(contact_names)) )
-            self.robot.foot_force[i] = 0.0
+            self.robot.foot_contact_pos[i] = []
+            self.robot.foot_force_each[i] = []
+            self.robot.foot_force_sum[i] = 0.0
             if self.ground_ids & contact_ids:
                 # see Issue 63: https://github.com/openai/roboschool/issues/63
                 # feet_collision_cost += self.foot_collision_cost
@@ -86,7 +88,9 @@ class WalkerBaseBulletEnv(BaseBulletEnv):
                 contact_ids = contact_list[0]
                 for x in f.contact_list():
                     if x[2] == contact_ids[0] and x[4] == contact_ids[1]:
-                        self.robot.foot_force[i] += x[9]
+                        self.robot.foot_contact_pos[i].append(x[6]) # contact position of the floor not foot
+                        self.robot.foot_force_each[i].append(x[9])
+                        self.robot.foot_force_sum[i] += x[9]
             else:
                 self.robot.feet_contact[i] = 0.0
 
--- pybulletgym/envs/roboschool/robots/locomotors/walker2d.py
+++ pybulletgym/envs/roboschool/robots/locomotors/walker2d.py
@@ -9,7 +9,9 @@ class Walker2D(WalkerBase, MJCFBasedRobot):
     def __init__(self):
         WalkerBase.__init__(self, power=0.40)
         MJCFBasedRobot.__init__(self, "walker2d.xml", "torso", action_dim=6, obs_dim=22)
-        self.foot_force = [0]*len(self.foot_list)
+        self.foot_force_each = [[]]*len(self.foot_list)
+        self.foot_force_sum = [0]*len(self.foot_list)
+        self.foot_contact_pos = [[]]*len(self.foot_list)
         self.foot_3d_orien = [np.array([0,0,0,1])]*len(self.foot_list)
         self.foot_2d_orien = [np.array([1,0])]*len(self.foot_list)
 
